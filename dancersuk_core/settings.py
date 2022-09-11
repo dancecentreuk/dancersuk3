edf246ba87d5c94849fdb8a10b3abee79489630d
DCUKM1 <dcukm1@dcukm1s-mbp.home>
2022-09-11 07:52:45 +0100

    email settings3

--- dancersuk_core/settings.py
+++ dancersuk_core/settings.py
@@ -182,7 +182,15 @@ MESSAGE_TAGS = {
 
 
 
+#email
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'dancecentreuk@gmail.com'
+EMAIL_HOST_PASSWORD = 'nssqgkodpzydtdmb'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
 
